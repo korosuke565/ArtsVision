c25fb1205ee713baf1c8cab50f1068b30cff4dcc
korosuke565 <t12619tt@sfc.keio.ac.jp>
2017-04-20 13:03:42 +0900

    add TensorSwift

---
+++ ArtsVision/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArtsVision
+//
+//  Created by TsuyoshiTonobe on 2017/04/20.
+//  Copyright © 2017年 TsuyoshiTonobe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/TensorSwift/TensorSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TensorSwift : NSObject
+@end
+@implementation PodsDummy_TensorSwift
+@end
